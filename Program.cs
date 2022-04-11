1007bac5c586062b208ef18c66a90db6d44768f7
wsr3 <wsr3@college.edu>
2022-04-11 13:37:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1; //задание 1
+            double b = 2; // вещественная переменная и присвоить ей любое значение
+            var c = 34;
+           
+        }
+    }
+}
